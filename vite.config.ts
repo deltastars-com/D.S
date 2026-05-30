b5250afb725c4fc4050ffe52b32c12af81053980
deltastars <deltastars90@gmail.com>
2026-05-30 23:13:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,13 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: false
-  }
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 });
