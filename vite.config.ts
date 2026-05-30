cc77542ef66e62e6cc85ef4505b76e90e0d5f9b5
deltastars <deltastars90@gmail.com>
2026-05-31 02:13:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [react()], // حذف tailwindcss() من هنا
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  plugins: [react()],
+  // هذا يمنع Vite من تعقيد عملية بناء الـ CSS
+  css: {
+    postcss: './postcss.config.js',
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
+  }
 });
