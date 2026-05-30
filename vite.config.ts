446f0290852de86f63f3bd4f2dde104b6f21c50f
deltastars <deltastars90@gmail.com>
2026-05-30 23:01:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -15,25 +14,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: false, // لتقليل حجم الملفات وزيادة الأداء
-    minify: 'terser', // استخدام Terser لضغط الملفات بأعلى كفاءة
-    terserOptions: {
-      compress: {
-        drop_console: true, // إزالة أي رسائل console.log من المتجر النهائي
-        drop_debugger: true,
-      },
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // تقسيم الملفات الكبيرة لتحسين سرعة التحميل
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
+    sourcemap: false
+  }
 });
