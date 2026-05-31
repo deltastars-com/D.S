0fd9ba926fd878f65d33d8e89b757cb92dbfc679
deltastars <deltastars90@gmail.com>
2026-05-31 03:07:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // هذا هو المحرك الرسمي لـ v4
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // سيعالج الـ CSS تلقائياً ولا يحتاج PostCSS
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
